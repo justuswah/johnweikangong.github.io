997a1eba809f388e4900eafbd5914e19174dd6d5
thegreatkang <ongwk95@hotmail.com>
2017-10-09 13:06:54 +0800

    Added content

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -81,8 +81,24 @@ jQuery(function ($) {
         })
     }());
 
+    $(window).on('scroll', function() {
 
+ 		var h = $('section').height();
+ 		var y = $(window).scrollTop();
+    var nav = $('#nav-wrap');
 
+ 	   if ( (y > h*.10) && (y < h) && ($(window).outerWidth() > 768 ) ) {
+ 	      nav.fadeOut('fast');
+ 	   }
+       else {
+          if (y < h*.20) {
+             nav.removeClass('opaque').fadeIn('fast');
+          }
+          else {
+             nav.addClass('opaque').fadeIn('fast');
+          }
+       }
+ 	});
 
     // -------------------------------------------------------------
     // Back To Top
